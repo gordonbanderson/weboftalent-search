8a9f3860dd35212082f73a5e5f98ba990de387e5
Gordon Anderson <gordon.b.anderson@gmail.com>
2013-01-16 21:11:59 +0700

    ENHANCEMENT: Enable Lucene

--- _config.php
+++ _config.php
@@ -4,4 +4,7 @@ FulltextSearchable::enable();
 
 // Add the SearchpageURI method, which checks for the existence of a page of class SearchPage
 Object::add_extension('Page_Controller', 'SearchPageExtension');
+
+Object::add_extension('SearchPage_Controller', 'ZendSearchLuceneContentController');
+
 ?>
