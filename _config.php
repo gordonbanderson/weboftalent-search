78e729b478843a8a72a7454f5107f2e06d8a3a5d
Gordon Anderson <gordon.b.anderson@gmail.com>
2014-11-12 12:44:43 +0700

    CONFIG: Move extension definition to yml

--- _config.php
+++ _config.php
@@ -1,7 +1,3 @@
 <?php
 // enable full text searching
 FulltextSearchable::enable();
-
-// Add the SearchpageURI method, which checks for the existence of a page of class SearchPage
-Object::add_extension('Page_Controller', 'SearchPageExtension');
-?>
